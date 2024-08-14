bca14322ce0f21b73121b1c7dc80d8a52598cfb8
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-08-14 10:26:43 +0500

    Conditional_Statements_if_else

---
+++ 02.DataTypes_Operators_Equility_and_Relational_Operators_Logical_Operators_Conditional_Statement_if_else/05.Conditional_Statements_if_else.dart
@@ -0,0 +1,19 @@
+// CONDITIONAL STATEMENTS (IF , ELSE):
+// if statement allows a block of code to be executed only when specfied conition
+// is ture . An if statement evaluates a Boolen Expression followed by
+// one or more statements. The Given Boolen Expreesion results in a Boolen value that can
+// only be either true or flase
+
+void main() {
+  int num5 = 12;
+  int num6 = 12;
+  if (num5 > num6) {
+    print("num1 is greater than num2");
+  } else if (num5 == num6) {
+    print("num1 is equal to num2");
+  } else {
+    print("num2 is greater than num1");
+  }
+  print("if else sab khatam hogaya, yee uske bad chal rha hai.");
+  print("Hello, SohAil MAnzoor");
+}
