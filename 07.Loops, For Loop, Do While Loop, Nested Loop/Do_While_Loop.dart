866fe5fbbf10910370d7611699d4e3545cd2072b
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-08-18 15:36:45 +0500

    Loops, For Loop, Do While Loop, Nested Loop

---
+++ 07.Loops, For Loop, Do While Loop, Nested Loop/Do_While_Loop.dart
@@ -0,0 +1,22 @@
+void main() {
+  // Do While Loop excute one statement after that it check condition
+
+  // // Infinate Loop  - Continuly Running 0
+  // int abcd = 0;
+  // while (abcd < 5) {
+  //   print(abcd);
+  // }
+
+  // Same Above Program but with Post Increment to Stop Loop
+
+  // int abcde = 0;
+  // while (abcde < 5) {
+  //   print(abcde);
+  //   abcde++;                 // with Post Increment To Stop Loop
+  // }
+
+  int abc = 0;
+  do {
+    print("Print once");
+  } while (abc > 5);
+}
