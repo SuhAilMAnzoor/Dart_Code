84c360b41d70f93003e24e13fcfb327efca38ff3
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-08-21 23:20:54 +0500

    Same Above Program, Make a Function of Two Numbers and Sum These Two Numbers With The Help Reutrn Type Function

---
+++ 08.Functions, Return Type, Without Return Type, void Type , Parameters, Required Parameter, Optional Parameter/Functions_05_ReturnType03.dart
@@ -0,0 +1,14 @@
+num addOfTwoNumbers() {
+  num num5 = 12;
+  num num6 = 5; // this return function return value in num
+  return num5 + num6; // return value
+}
+
+void main() {
+  var result =
+      addOfTwoNumbers(); // Assigning fuction's return value into variable result
+  print(result); // printing the result value
+
+  print(
+      addOfTwoNumbers()); // also you can print directly function without storing
+}
