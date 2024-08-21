1f6e66974b42487c31518e0d819123aa72c62c56
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-08-21 17:39:24 +0500

    Without Return Type Function

---
+++ 08.Functions, Return Type, Without Return Type, void Type , Parameters, Required Parameter, Optional Parameter/Functions_03_ReturnType01WithoutReturn.dart
@@ -0,0 +1,11 @@
+sumOfTwoNumbers() {
+  num num1 = 12;
+  num num2 = 4; // Without Return
+  print(num1 + num2);
+}
+
+void main() {
+  var a =
+      sumOfTwoNumbers(); // a variable is not getting value from this function
+  print(a); //>> Null             //  Not return value of function in a
+}
