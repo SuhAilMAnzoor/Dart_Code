6d6d9c02823ad6b95d7ec7bebf18732645530490
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-08-21 23:11:20 +0500

    Return Type of Fuction with Return keyword

---
+++ 08.Functions, Return Type, Without Return Type, void Type , Parameters, Required Parameter, Optional Parameter/Functions_04_ReturnType02WithRetrun.dart
@@ -0,0 +1,11 @@
+SumofTwoNumbers() {
+  num num3 = 12;
+  num num4 = 5; // With Return
+  return num3 + num4;
+}
+
+void main() {
+  var b = SumofTwoNumbers(); // variable b is getting value from the fuction
+  print(
+      b); //>> 17                     // The Fuction returns sum value to variable b
+}
