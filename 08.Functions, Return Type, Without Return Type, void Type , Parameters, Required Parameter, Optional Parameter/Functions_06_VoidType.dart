abf60f31021199ddfccde0e9f3244dbef580d3b7
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-08-22 10:13:42 +0500

    Void Type Function, a void fuction perform actions like printing , modifying variables, and others, but does not return value

---
+++ 08.Functions, Return Type, Without Return Type, void Type , Parameters, Required Parameter, Optional Parameter/Functions_06_VoidType.dart
@@ -0,0 +1,20 @@
+//|||||||Functions =====  VOID TYPE|||||
+
+// A function with a void return type is a function
+// that performs an action but doesn't return any data to the calling code.
+
+//Void functions do not return a value.
+//They commonly perform actions such as printing, modifying variables, and interacting with systems.
+//They can contain control flow, error handling, and event handling logic.
+//Void functions are used for initialization, cleanup, and event-driven programming.
+//Their purpose isn't limited to actions; they can encompass a wide range of functionalities based on program requirements.
+
+void greet() {
+  print("Hi There");
+}
+
+void main() {
+  //call greet function in main function
+  greet(); //This will shows what its action
+  //Output >> Hi There
+}
