196d949e5ee77e05c1d5d12bedc4a47ec31a0c97
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-08-22 18:14:33 +0500

    Make a Void Function to Sum of two numbers

---
+++ 08.Functions, Return Type, Without Return Type, void Type , Parameters, Required Parameter, Optional Parameter/Functions_07_VoidType01.dart
@@ -0,0 +1,10 @@
+void SumofTwoNumbers() {
+  num num3 = 12;
+  num num4 = 5;
+  var a = num3 + num4; // void only perform a certin action
+  print(a); // does not return value
+}
+
+void main() {
+  SumofTwoNumbers(); //calling the function in main function
+}
