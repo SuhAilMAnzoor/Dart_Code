40a494b4f1a912e13cbe9ab2abf358681ea07a4c
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-08-23 17:22:48 +0500

    Sum Of two numbers get the value using parameters

---
+++ 08.Functions, Return Type, Without Return Type, void Type , Parameters, Required Parameter, Optional Parameter/Functions_09_With_Parameters.dart
@@ -0,0 +1,20 @@
+//  ======================FUNCTION WITH PARAMETERS======================||
+
+// EXAMPLE OF PARAMETER
+
+void main() {
+  print(sumOfTwoNumber(2, 4)); // its required parameter
+  print(sumOfTwoNumber(5, 4));
+}
+
+// function defined here using parameteres
+num sumOfTwoNumber(int num1, int num2) {
+  num Result = num1 + num2;
+  return Result;
+}
+
+
+
+
+//>> TYPES OF PARAMETERS
+//>> PARAMETERS CAN BE OPTIONAL AND REQUIRED  and i guess 2 other.
