1023f48f59cd04c9de7860f70c39268be8a38855
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-08-27 17:33:48 +0500

    Student Marksheet Program, Ask student name and subjects calculate them and print the percentage the student is fail or pass, without main function you have to call it in main function

---
+++ 08.Functions, Return Type, Without Return Type, void Type , Parameters, Required Parameter, Optional Parameter/StudentsMarkSheet.dart
@@ -0,0 +1,17 @@
+//Function Defined Here,but you have to call on Main Function
+studentsMarkSheet() {
+  //Functions
+  //This Program  is without Functions    Program 1
+
+  String name = "Sohail";
+  num subject1 = 76;
+  num subject2 = 65;
+  num subject3 = 87;
+  num obtain_marks = subject1 + subject2 + subject3;
+  num percentage = (obtain_marks / 300) * 100;
+  if (percentage > 50) {
+    print("$name pass hogaya");
+  } else {
+    print("$name Fail Hogaya");
+  }
+}
