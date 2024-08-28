6114dd27a77d00baf2518a1058390bfef23e4d32
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-08-28 16:22:13 +0500

    This is Previous Class Revision, This is a Required Function

---
+++ 09.Arrow Function, Null Safety, Ternary Operator/FunctionPreviousClassRevision_1.dart
@@ -0,0 +1,22 @@
+// Revision of previous class
+
+// This is Required Function, you have to provide both
+// agremunts in sequence order if not then you will,
+// get error if you not provide and
+// order matters like first is name so you have to provide
+// first name and then after city name,
+// dont change sequence !
+// By Default My thought is this function is required function
+
+// Function defined here
+studentInfo(String name, String city) {
+  print("Name : $name");
+  print("City : $city");
+}
+
+void main() {
+  studentInfo("Sohail", "Badah"); //Sequence Matter
+
+//  //Not Like this, it is not in sequence order
+//  studentInfo("Badah", "Sohaill");
+}
