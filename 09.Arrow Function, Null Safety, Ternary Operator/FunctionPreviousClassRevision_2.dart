e28b9a98600e6f8fe4d7604fe19d66123584d041
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-08-28 16:25:01 +0500

    This is Previous Class Revision, This is Optional and Named Function

---
+++ 09.Arrow Function, Null Safety, Ternary Operator/FunctionPreviousClassRevision_2.dart
@@ -0,0 +1,26 @@
+// Revision of previous class
+
+// This is Optional plus named Function we use
+// curely braces <{}> for this,if you don't not
+// pass any arguments then you will not get any error
+// and you have not to worry to provide detail in sequence
+// order like first is name so you have to provide first
+// name it does not matter because it will helighted named
+// field
+
+void main() {
+  StudentInfo(); // there is no any error, i didn't pass any arguments
+  // both are optional, and named
+  //
+}
+
+// Function defined here
+StudentInfo({name, city}) {
+  print("Name : $name");
+  print("City : $city");
+  //i use curely braces inside the round brackets i have already removed
+  // datatypes for removing errors in function we will discuss these
+  // errors in null safety (we will in this lecture)
+  // to handle these errors
+  // to keep or prevent datatypes with (we will learn in this lecture)
+}
