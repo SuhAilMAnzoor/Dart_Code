04da2d98fc9bfbbc20f785243c65384b6cf76489
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-08-30 17:48:33 +0500

    Array Function in Dart, if you have single line of code you Arrow Function not Necessary but helps to reduce code

---
+++ 09.Arrow Function, Null Safety, Ternary Operator/ArrowFunction_03.dart
@@ -0,0 +1,23 @@
+void main() {
+  // call and print the function
+  print(SumofTwoNumberss(4, 5));
+}
+
+// if you have single line of code in function like you
+// want to perform some calculations or operation if those
+// actions are single line code so for that we use arraw
+// function for that one it is not necessary to use arrow
+// function but it helps to reduce the code and return the value
+// , we mainly create the structure before this
+// like below function at the bottom commited function
+
+//Arrow Function
+// This Arrow (=>) Replace Curely Braces And Return the value of Single Statment.
+//Arrow Function is used where we have single line of code in curely braces
+SumofTwoNumberss(num1, num2) => num1 + num2;
+
+
+// //Without Arrow Function
+// SumofTwoNumberss(num1, num2) {
+//   return num1 + num2;
+// }
