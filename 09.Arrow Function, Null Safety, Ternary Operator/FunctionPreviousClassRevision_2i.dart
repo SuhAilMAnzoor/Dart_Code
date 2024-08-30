2c7e948f5d772b281a58ab04a523dc49ebd2216f
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-08-30 17:38:35 +0500

    This is Optional plus named function, Understanding Optional and Required.

--- 09.Arrow Function, Null Safety, Ternary Operator/FunctionPreviousClassRevision_2i.dart
+++ 09.Arrow Function, Null Safety, Ternary Operator/FunctionPreviousClassRevision_2i.dart
@@ -5,7 +5,7 @@
 // This is Optional plus named Function we use
 // curely braces <{}> for this,if you don't not
 // pass any arguments then you will not get any error
-// and you have not to worry to provide detail in sequence
+// and you have no worries to provide detail in sequence
 // order like first is name so you have to provide first
 // name it does not matter because it will helighted named
 // field
