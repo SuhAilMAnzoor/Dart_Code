0be37f71b2eb5053c9d6be78f3db02527878dd20
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-08-31 19:16:01 +0500

    in this file defining function there is no error, because it is required function so any how you have to give values this function gets name value

---
+++ 09.Arrow Function, Null Safety, Ternary Operator/NullSafety_04iii_AcutllyNullSafetyExample.dart
@@ -0,0 +1,11 @@
+void main() {}
+
+StudentInfo(String name) {}
+
+
+// No Error Here 
+//Becuse this Required Function You must have to 
+//give values
+
+
+//But Come To Next Code which same as this one 
