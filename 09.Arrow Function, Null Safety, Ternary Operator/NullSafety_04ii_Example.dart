ab4c9718ef19f75df18567cc5ced2966cae8008b
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-08-31 19:07:58 +0500

    Another examples to Understand Null Safety, abc has <null> not a value for storing

---
+++ 09.Arrow Function, Null Safety, Ternary Operator/NullSafety_04ii_Example.dart
@@ -0,0 +1,7 @@
+void main() {
+  var abc;
+  String Email = abc; //Assigned to variable Email that is String
+  print(Email);
+
+  print(abc); //abc has no value it has null that is not value for storing
+}
