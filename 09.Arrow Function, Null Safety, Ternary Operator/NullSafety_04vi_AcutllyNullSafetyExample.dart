40aa85e2ccaad613c786916c337993a6d72c92ae
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-09-02 00:06:40 +0500

    Exclaminatuion mark   // Adding <!> Mark at the end input or variable name

---
+++ 09.Arrow Function, Null Safety, Ternary Operator/NullSafety_04vi_AcutllyNullSafetyExample.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main() {
+  String name = stdin.readLineSync()!;
+
+  // Adding <!> Mark at the end input or variable name
+  // you set it that you take input in String not null Value
+  // you can't store  null value
+  // this will take input only on String
+  //
+  //
+  // so you saw exclamination mark !
+  // set the value of string
+  // or even other datatypes you can use it
+
+  //type lagana best approach hai
+}
