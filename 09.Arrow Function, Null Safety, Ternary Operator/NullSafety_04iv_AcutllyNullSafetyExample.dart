664a1002d1da568d01817c4e82edc65a5d5e4613
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-09-01 23:57:59 +0500

    More Examples to Understand Null Safety

---
+++ 09.Arrow Function, Null Safety, Ternary Operator/NullSafety_04iv_AcutllyNullSafetyExample.dart
@@ -0,0 +1,12 @@
+void main() {}
+
+StudentInfo({String name}) {}
+
+
+// Same Code as Prevoius one, added Curely Braces
+// inside the Round Brackets Before <String> and after <name>
+// but it gives Error, Why ?
+// <name> variable is helighted red 
+// means it is optional parameter <name> can't have a value
+// of 'null' because of its type, but the implicit default value 
+// is 'null;.
