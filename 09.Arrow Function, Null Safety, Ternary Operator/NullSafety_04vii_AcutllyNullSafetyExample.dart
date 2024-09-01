c5856880d265180a80921d576b9de795865fad90
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-09-02 00:32:27 +0500

    // We Add Question Mark <?> At the End Of Datatype

---
+++ 09.Arrow Function, Null Safety, Ternary Operator/NullSafety_04vii_AcutllyNullSafetyExample.dart
@@ -0,0 +1,15 @@
+// Now Avoid this error which you saw in this file
+// NullSafety_04iv_AcutllyNullSafetyExample.dart
+
+// StudentInfo({String name}){
+
+// }
+//==============================================
+
+// We Add Question Mark <?> At the End Of Datatype
+// Question Mark Ka Matlab ye hai ke Null Ho Bhi Skta Hai
+// Nahe Bhi Ho Skta hai
+
+StudentInfo({String? name}) {}
+
+void main() {}
