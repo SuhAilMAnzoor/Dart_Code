43ba4339f31092af4fcb3feea89b21153fb3f028
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-09-06 00:07:22 +0500

    make a bool program in Null Safety to check what ! mark does and ? mark as well for better understanding

---
+++ 09.Arrow Function, Null Safety, Ternary Operator/NullSafety_05_AcutllyNullSafetyExampleviiii.dart
@@ -0,0 +1,10 @@
+// Another Example on it
+
+void main() {
+  StudentsInfo(resourceKoKamAtaHaiYaNahi: true);
+}
+
+StudentsInfo({bool? resourceKoKamAtaHaiYaNahi}) {
+  bool officeKaCeo = resourceKoKamAtaHaiYaNahi!;
+  print(officeKaCeo);
+}
