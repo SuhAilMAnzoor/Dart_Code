4f395691e659a64457e8031fe795b10f0e9f0178
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-09-06 00:30:14 +0500

    If age is greater than 18 , print : Allow , if age is less than print : Not Allowed with Ternary Operator

---
+++ 09.Arrow Function, Null Safety, Ternary Operator/TernaryOperator06.dart
@@ -0,0 +1,13 @@
+void main() {
+  int age = 19;
+  String result = age >= 18 ? "Allow" : "Not Allowed";
+
+  //Simple if else ho rha hai
+}
+
+
+// String ban lo ya bool ban lo 
+
+// NOTE; This Question Mark ? is Not Null Safety 
+// This Topic is Completely Different From Null Safety 
+// This is Single line Simple If Else Hai
