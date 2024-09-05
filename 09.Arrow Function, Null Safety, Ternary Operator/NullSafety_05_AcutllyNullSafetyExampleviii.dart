2d72d0437aa60745ecb99a04a53b849713610057
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-09-05 23:48:21 +0500

    ! Mark denotes FB link will come surely, but i didnt pass any parameter in above called function

---
+++ 09.Arrow Function, Null Safety, Ternary Operator/NullSafety_05_AcutllyNullSafetyExampleviii.dart
@@ -0,0 +1,14 @@
+// Another Example on it
+
+void main() {
+  // Here i am giving Fblink
+  StudentsInfo();
+}
+
+StudentsInfo({String? FBlink}) {
+  String abc = FBlink!;
+  print(abc);
+}
+
+// abhi program phatega q ke mane lazm keya ke
+// fblink milgi ! mark laga ke
