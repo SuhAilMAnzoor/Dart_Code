c4bb4aac029b2b01a26d824f9846064366fdd413
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-09-09 19:00:21 +0500

    Print Stars Pattern Project - Console based

---
+++ 10. Practice Projects/practice_project_1.dart
@@ -0,0 +1,22 @@
+//You have to print this
+
+// *
+// **
+// ***
+// ****
+
+import 'dart:io';
+
+void main() {
+  for (var i = 1; i < 5; i++) {
+    for (var j = 1; j <= i; j++) {
+      stdout.write("*");
+    }
+    print("");
+  }
+}
+
+// These Kind Of Programs Will Boost your logic
+// Please Do These Kind Of Programs
+// Put Your Full Foucs to Understand
+// These Programs are very hard
