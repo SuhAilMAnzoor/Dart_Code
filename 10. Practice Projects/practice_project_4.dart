20e712491a68a58ebe535964ea8e8599e8f30e1d
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-09-09 19:37:01 +0500

    print 2 numbers in each row then add '==' space after

---
+++ 10. Practice Projects/practice_project_4.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+
+void main() {
+  var rows = 5;
+  var counter = 1;
+  var a = " ";
+  for (var i = 1; i < rows; i++) {
+    for (var j = 1; j <= 2; j++) {
+      print(counter++);
+    }
+    print("==");
+  }
+}
+
+//You have to print this
+
+//       1 
+//       2
+//       == 
+//       3
+//       4
+//       ==
+//       5
+//       6
+//       ==
+//       7
+//       8
+//       ==
+
+// These Kind Of Programs Will Boost your logic
+// Please Do These Kind Of Programs
+// Put Your Full Foucs to Understand
+// These Programs are very hard
