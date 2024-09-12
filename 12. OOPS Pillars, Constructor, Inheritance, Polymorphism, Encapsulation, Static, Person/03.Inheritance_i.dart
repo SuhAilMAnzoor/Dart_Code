a01531c2c967c5a27d5a17f72b09eb8a832816e1
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-09-12 22:47:25 +0500

    Inheritance i

---
+++ 12. OOPS Pillars, Constructor, Inheritance, Polymorphism, Encapsulation, Static, Person/03.Inheritance_i.dart
@@ -0,0 +1,32 @@
+void main() {}
+
+class Person {
+  String? name;
+  int? age;
+
+  displayInfo() {
+    print("Name : $name"); // Repeat the Code
+    print("Age : $age");
+  }
+}
+
+class Student {
+  String? name;
+  int? age;
+
+  displayInfo() {
+    print("Name : $name"); //Again Repeat the Code
+    print("Age : $age");
+  }
+}
+
+class Teacher {
+  String? name;
+  int? age;
+
+  displayInfo() {
+    // Again Repeat the Same Code
+    print("Name : $name");
+    print("Age : $age");
+  }
+}
