f20ae2f165b64f6f850b541b3e1726c6a877d9fd
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-09-12 22:40:14 +0500

    Default Constructor

---
+++ 12. OOPS Pillars, Constructor, Inheritance, Polymorphism, Encapsulation, Static, Person/02.Constructor_Default.dart
@@ -0,0 +1,21 @@
+//Default Constructor
+
+void main() {
+  Person obj = Person();
+}
+
+class Person {
+  String? namee;
+  int? agee;
+
+  Person() {
+    print("Class Call Hogai");
+    // Default Constrcuctor >> is lya use hota hai ke class call hote
+    // Constrcutor call hojata hai na wo jo class ke call hone pe koi koi
+    // perform hota hai na to hum wo construcotr men kart hai
+  }
+}
+
+
+ // Remining Named and Factori Constrcutor we will see them
+ // in flutter
