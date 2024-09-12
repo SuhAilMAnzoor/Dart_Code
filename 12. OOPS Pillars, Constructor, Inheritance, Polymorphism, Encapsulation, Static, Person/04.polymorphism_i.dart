9647cbb9b099865dcd112ab48afed1f20c65873d
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-09-12 23:04:46 +0500

    Polymorphism

---
+++ 12. OOPS Pillars, Constructor, Inheritance, Polymorphism, Encapsulation, Static, Person/04.polymorphism_i.dart
@@ -0,0 +1,13 @@
+  // Polymorphism
+  // Polymorphism has two types 
+  //1. Overloading
+  //2. Overriding
+
+//Polymorphism in Dart refers to the ability of objects of different classes to be treated
+//as objects of a common superclass. This allows you to write flexible code that can work with
+//various objects without knowing their exact types at compile time. There are two main ways to achieve polymorphism in Dart:
+
+//Method Overriding: This is a core concept in inheritance. When a subclass inherits from a parent class,
+//it can redefine methods inherited from the parent class to provide its own implementation.
+//This allows you to have a single method name with different behaviors depending on the object's actual type.
+
