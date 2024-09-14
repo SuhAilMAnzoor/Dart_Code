149bb9ecf7940a68ec0a6d86858ffe6ac52a08b8
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2024-09-14 22:47:49 +0500

    Making it private class

---
+++ 12. OOPS Pillars, Constructor, Inheritance, Polymorphism, Encapsulation, Static, Person/person.dart
@@ -0,0 +1,10 @@
+// In this file i am making it private class
+
+class Person {
+  String name = "Ahmed";
+  String _bankDetails = "abc-abc-abc";
+
+  displayInfo() {
+    print("Name : $name");
+  }
+}
